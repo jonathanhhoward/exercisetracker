b177ca648a55171676b113ca8fde767d03559e56
slipskid <2773262@gmail.com>
2019-12-07 13:36:55 -0500

    Reorder

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
 require('dotenv').config()
 
-const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
-
+const express = require('express')
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).catch(err => console.error('connection error:', err))
 
-mongoose.connection
-  .on('error', console.error.bind(console, 'connection error:'))
-  .once('open', () => {
-    console.log('Mongoose connected')
-  })
-
-app.use(cors())
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-})
+app.use(cors())
 
-// Not found middleware
 app.use((req, res, next) => {
   return next({ status: 404, message: 'not found' })
 })
 
-// Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage
 
   if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
+    errCode = 400
     const keys = Object.keys(err.errors)
-    // report the first validation error
     errMessage = err.errors[keys[0]].message
   } else {
-    // generic or custom error
     errCode = err.status || 500
     errMessage = err.message || 'Internal Server Error'
   }
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(err => console.error('new connection error:', err))
+
+mongoose.connection
+  .on('error', console.error.bind(console, 'connection error:'))
+  .once('open', () => {
+    console.log('Mongoose connected')
+  })
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(
